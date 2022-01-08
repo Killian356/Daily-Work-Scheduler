b0f9ff3d2347d92cd43b7437d38ac21aa0a3f91e
Killian Chamberlain <killian7195@gmail.com>
2022-01-07 18:07:55 -0600

    Adding major function webpage save and refresh functions

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+  var $today = $("#today");
+  var notes = getNotes();
+  var interval = setInterval(setTime, 07);
+
+  updateBackground();
+  renderNotes();
+
+  // Displays Current Day "today" in header
+  function setTime() {
+    var date = moment().format("dddd, MMMM Do YYYY");
+    $today.innerHTML = date;
+    $("#today").text(date);
+  }
+
+  // Saves written data into local storage
+
+  $(".saveBtn").on("click", function (e) {
+    e.preventDefault();
+    var val = $(this).siblings(".description").val();
+    var hour = $(this).siblings(".description").attr("id");
+    notes[hour] = val;
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+  });
+
+  $(".clear").on("click", function (e) {
+    e.preventDefault();
+    $(".description").val(" ");
+    var notes = {};
+    localStorage.setItem("notes", JSON.stringify(notes));
+  });
+
+  // Retrieving data from local storage, converting notes to stringify
+  
+  function getNotes() {
+    var notes = localStorage.getItem("notes");
+    if (notes) {
+      notes = JSON.parse(notes);
+    } else {
+      notes = {};
+    }
+    return notes;
+  }
+
+  // looping
+  function renderNotes() {
+    for (var key in notes) {
+      $("#" + key).val(notes[key]);
+    }
+  }
+
+  //  Past, present, future functions to page
+  function updateBackground() {
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).children(".description").attr("id"));
+      $(this)
+        // remove classes
+        .removeClass("present")
+        .removeClass("past")
+        .removeClass("future");
+
+      if (blockHour === currentHour) {
+        $(this).addClass("present");
+      }
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      }
+      if (blockHour > currentHour) {
+        $(this).addClass("future");
+      }
+    });
+  }
+});
